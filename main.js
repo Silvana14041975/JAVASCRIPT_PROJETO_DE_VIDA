258d3124177e9d12197b3438e82dc448ad765664
Silvana Gandra <115194785+Silvana14041975@users.noreply.github.com>
2024-03-13 05:32:48 -0300

    atualização

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba.conteudo");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function (){
@@ -16,14 +16,31 @@ for(let i=0;i <botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-05T00:00:00");
 
-contadores [0].textContent = calculaTempo("tempoObjetivo1")
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
-function calculaTempo(tempoObjetivo1){
+
+for (let i=0; i<contadores.length;i++){
+    contadores[i].textContent = calculaTempo(tempos[1]);
+}
+
+
+function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date ();
     let tempoFinal = tempoObjetivo1 - tempoAtual;
-    let segundos = tempoFinal/1000;
-    return segundos;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
 }
 
 
